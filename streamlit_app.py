c9330cd72030e9527add1a8e462009960e375d46
JimmyKPMG <155457069+JimmyKPMG@users.noreply.github.com>
2024-01-25 11:58:07 +0100

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def get_fruiyvice_data(this_fruit_choice) :
     return fruityvice_normalized
 
 def get_fruit_load_list() :    
-        with my_cnx.cursor() as my my_cur :
+        with my_cnx.cursor() as my_cur :
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 streamlit.header('Fruityvice Fruit Advice!')
