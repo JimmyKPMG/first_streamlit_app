586bd0998691563cc74b8be7c4432994efbc32c1
JimmyKPMG <155457069+JimmyKPMG@users.noreply.github.com>
2024-01-25 11:58:40 +0100

    Update streamlit_app.py
    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def get_fruiyvice_data(this_fruit_choice) :
 
 def get_fruit_load_list() :    
         with my_cnx.cursor() as my_cur :
-        my_cur.execute("select * from fruit_load_list")
+         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 streamlit.header('Fruityvice Fruit Advice!')
 try :
